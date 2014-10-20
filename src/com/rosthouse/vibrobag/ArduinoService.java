bf47bdae201309fa80ed016f0cf37933a5946b3e
Rosthouse <rosthouse@gmail.com>
2014-10-20 19:25:53 +0200

    Reworked Bluetooth app, includes services wich handle the connection in the background. Doesn't work completly yet, have to read up on services, intents and stuff

--- src/com/rosthouse/vibrobag/ArduinoService.java
+++ src/com/rosthouse/vibrobag/ArduinoService.java
@@ -1,6 +1,6 @@
 package com.rosthouse.vibrobag;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import static android.content.ContentValues.TAG;
@@ -8,13 +8,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import java.util.UUID;
 
-public class ArduinoService extends IntentService {
+public class ArduinoService extends Service {
 
     public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
     public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
@@ -26,38 +27,37 @@ public class ArduinoService extends IntentService {
     private UUID uuid = UUID.randomUUID();
     private BroadcastReceiver broadcastMessageReceiver;
 
-    public ArduinoService() {
-        super(ArduinoService.class.getName());
+    ArduinoServiceBinder binder = new ArduinoServiceBinder();
+
+    public class ArduinoServiceBinder extends Binder {
+
+        public ArduinoService getService() {
+            return ArduinoService.this;
+        }
     }
 
-    // Handles various events fired by the Service.
-    // ACTION_GATT_CONNECTED: connected to a GATT server.
-    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
-    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
-    // ACTION_DATA_AVAILABLE: received data from the device.  This can be a result of read
-    //                        or notification operations.
+    /**
+     * Handles various events fired by the Service. ACTION_GATT_CONNECTED:
+     * connected to a GATT server. ACTION_GATT_DISCONNECTED: disconnected from a
+     * GATT server. ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+     * ACTION_DATA_AVAILABLE: received data from the device. This can be a
+     * result of read or notification operations.
+     */
     private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
             if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
                 mConnected = true;
-//                updateConnectionState(R.string.connected);
-//                invalidateOptionsMenu();
             } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
                 mConnected = false;
-//                updateConnectionState(R.string.disconnected);
-//                invalidateOptionsMenu();
-//                clearUI();
+                stopSelf();
             } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
-                // Show all the supported services and characteristics on the user interface.
-//                displayGattServices(mBluetoothLeService.getSupportedGattServices());
             } else if (BluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)) {
                 handleData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
-
-//                displayData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
             }
         }
+
     };
 
     // Code to manage Service lifecycle.
@@ -94,20 +94,8 @@ public class ArduinoService extends IntentService {
         Context context = getApplicationContext();
         CharSequence text = "Hello toast!";
         int duration = Toast.LENGTH_SHORT;
-
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
-    }
-    private static int DISCOVERY_REQUEST = 1;
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        String mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
-        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
-        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
-
-        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
-        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
     }
 
     private static IntentFilter makeGattUpdateIntentFilter() {
@@ -126,6 +114,22 @@ public class ArduinoService extends IntentService {
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        String mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
+        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        return super.onStartCommand(intent, flags, startId); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
